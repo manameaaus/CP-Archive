6fb0f452cea51939e222f50bd7dcb186b5342fe7
Aaus <80692936+manameaaus@users.noreply.github.com>
2025-11-26 10:08:52 +0530

    feat: PATCH DELETE /genres

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.post('/api/v1/book', async (req,res) => {
     res.status(201).send("Created");
 })
 
-app.get('/api/v1/genre', async (req,res) => {
+app.get('/api/v1/genres', async (req,res) => {
     const result = await prisma.genre.findMany({})
     res.status(200).send(result);
 })
 
-app.post('/api/v1/genre', async (req,res) => {
+app.post('/api/v1/genres', async (req,res) => {
     const name = req.body.name;
     const url = req.body.url;
 
@@ -29,6 +29,30 @@ app.post('/api/v1/genre', async (req,res) => {
     res.status(201).json(result);
 })
 
+app.patch('/api/v1/genres/:genreId', async (req,res) => {
+    const id = req.params.genreId;
+    const updatedFields = req.body;
+
+    const result = await prisma.genre.update({
+        where: {
+            id : Number(id)
+        },
+        data: updatedFields
+    })
+
+    res.status(200).json(result);
+})
+
+app.delete('/api/v1/genres/:genreId', async (req,res) => {
+    const id = req.params.genreId;
+    const result = await prisma.genre.delete({
+        where:{
+            id : Number(id)
+        }
+    })
+
+    res.status(204).send();
+})
 
 
 
