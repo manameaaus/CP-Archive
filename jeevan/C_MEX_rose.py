d277a490a3fdefc731d74bf8a1cd68ab02fdb7e5
JBR-0100 <jbr.the.programmer@gmail.com>
2026-03-13 09:11:00 +0530

    daily problems

---
+++ jeevan/C_MEX_rose.py
@@ -0,0 +1,11 @@
+def mex(arr,n):
+    st = 0
+    sss = set(arr)
+    for i in range(n+1):
+        if sss not in 
+t = int(input())
+for i in range(t):
+    n = int(input())
+    arr = list(map(int,input().split()))
+
+
