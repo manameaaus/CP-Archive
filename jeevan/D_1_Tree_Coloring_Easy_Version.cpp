21867b99b6a7a7871aabe31f26bc2ad97c94211f
JBR-0100 <jbr.the.programmer@gmail.com>
2026-03-16 09:17:00 +0530

    solve problem

---
+++ jeevan/D_1_Tree_Coloring_Easy_Version.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- debug utilities ----------
+template<typename... Args>
+
+// ---------- shortcut macros ----------
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define len(x) (int)(x).size()
+#define pb push_back
+#define print1(zzz) for(auto zzzz: zzz) cout << zzzz << ' ';cout<<endl;
+#define print2(zzz) for (auto &zzzzz : zzz) {print1(zzzzz)}
+#define space cout << endl;
+#define sum(v) (accumulate((v).begin(), (v).end(), 0LL))
+#define execute ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define per1(i, a, b) for (int i = (b); i >= (a); --i)
+void print(Args&&... args) {((cout << args << " "), ...);cout << '\n';}
+
+// ----------- constants -----------
+#define PI 3.14159265358979323846
+#define INF 1e18
+#define MOD 1000000007
+#define MOD2 998244353
+
+// ---------- type aliases ----------
+#define ll long long
+#define int long long
+#define vi vector<int> 
+#define vc vector<char> 
+#define vs vector<string> 
+#define vvc vector<vector<char>> 
+#define vvi vector<vector<int>> 
+#define pii pair<int, int> 
+
+// ---------- input macros ----------
+#define invi(v, n) vi v(n); rep(i, n) cin >> v[i]
+#define invs(v, n) vs> v(n); rep(i, n) cin >> v[i]
+#define rdvi(v) for (auto &x : v) cin >> x
+#define rdvs(v) for (auto &s : v) cin >> s
+#define msb(x) (x == 0 ? -1 : 1<<(63 - __builtin_clzll(x)))      
+#define lsb(x) (x == 0 ? -1 : 1<<(__builtin_ctzll(x)))   
+#define pc(x) (__builtin_popcountll(x))
+string d2b(long long num, ll pad = 0) { if (num == 0) return string(max(1LL, pad), '0');string res;while (num) {res += '0' + (num & 1);num >>= 1;}reverse(res.begin(), res.end());if (len(res) < pad) res = string(pad - res.size(), '0') + res;return res;}
+
+vector<vector<int>> adj;
+vector<vector<int>> dist;
+
+void dfs(int node, int par, int d) {
+    dist[d].push_back(par);
+    for (int nbr : adj[node]) {
+        if (nbr != par) {
+            dfs(nbr, node, d + 1);
+        }
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    adj.assign(n + 1, vector<int>());
+    dist.assign(n, vector<int>());
+
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(1, 0, 0);
+
+    int ans = 0;
+    for (auto &lis : dist) {
+        if (lis.empty()) continue;
+
+        set<int> s(lis.begin(), lis.end());
+        ans = max(ans, (int)lis.size() + (s.size() == 1));
+    }
+
+    cout << ans << '\n';
+}
+
+int32_t main() {
+    execute
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
