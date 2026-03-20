7d26c91ebf939ce9414cde3eca59343df2f7743a
JBR-0100 <jbr.the.programmer@gmail.com>
2026-03-20 13:02:00 +0530

    solve problem

---
+++ jeevan/J_The_Colored_Balls_Picking_Problem.py
@@ -0,0 +1,28 @@
+# def nck(n,k):
+#     deno = 1
+#     for i in range(k):
+#         deno = denon - i 
+#         deno //= (i+1)
+#     return deno
+# MOD = 1000000007
+# n = int(input())
+# a = []
+# for i in range(n):
+#     a.append(int(input()))
+
+# # suff = a[::-1]
+# # for i in range(n-2,-1,-1):
+# #     suff[i] += suff[i+1]
+
+
+# tot = sum(a)
+
+# res = 1
+# for i in range(n-1,-1,-1):
+#     res = res * nck(tot-1,a[i]-1) % 
+
+
+
+
+# # 3c2 * 2c2 * 1c1
+# 7 * 10 * 20 
